1b86d9b2b348adf0b3c2322e46c324aab35c8b1b
Michael Basov <michael.m.basov@gmail.com>
2020-12-22 23:06:10 -0500

    Basics of the game working. No lives/win/loss conditions. But controls are there.

---
+++ Assets/BallController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public Rigidbody2D rigidBody;
+    public float speed = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        moveBall(new Vector2(0f, -1f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        // Hit the Racket?
+        if (col.gameObject.name == "Paddle")
+        {
+            // Calculate hit Factor
+            float hitFactor = getHitFactor(transform.position, col.transform.position, col.collider.bounds.size.x);
+
+            // Calculate direction, set length to 1
+            moveBall(new Vector2(hitFactor, 1).normalized);
+        }
+    }
+        private void moveBall(Vector2 direction)
+    {
+        rigidBody.velocity= (direction * speed);
+    }
+    private float getHitFactor(Vector2 ballPos, Vector2 racketPos,
+                float racketWidth)
+    {
+        return (ballPos.x - racketPos.x) / racketWidth;
+    }
+}
---
+++ Assets/PaddleController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleController : MonoBehaviour
+{
+    public Camera MainCamera; //be sure to assign this in the inspector to your main camera
+    private Vector2 screenBounds;
+    private float objectWidth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenBounds = MainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, MainCamera.transform.position.z));
+        objectWidth = transform.GetComponent<SpriteRenderer>().bounds.extents.x; //extents = size of width / 2
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var mousePosition = MainCamera.ScreenToWorldPoint(Input.mousePosition);
+        var newX = Mathf.Clamp(mousePosition.x, (screenBounds.x * -1) + objectWidth, screenBounds.x - objectWidth);
+        transform.position = new Vector3(newX, transform.position.y);
+    }
+}
