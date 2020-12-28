7fbf8b2fdbba48bb4b2c632febca408f1d620e9e
Michael Basov <michael.m.basov@gmail.com>
2020-12-27 20:01:18 -0500

    Added a rudimentary Game Over screen. When the player runs out of lives the Game Over screen pops up and prompts them to restart the game.

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -11,6 +12,8 @@ public class GameManager : MonoBehaviour
     private int score=0;
     private int lives = 3;
     public GameObject ballPrefab;
+    public Canvas mainCanvas;
+    public Canvas gameOverCanvas;
 
     // Start is called before the first frame update
     void Start()
@@ -36,6 +39,7 @@ public class GameManager : MonoBehaviour
     }
     public void LoseALife(BallController ball)
     {
+        ball.destroyBall();
         AddToLives(-1);
         if (lives < 1)
         {
@@ -43,14 +47,19 @@ public class GameManager : MonoBehaviour
         }
         else
         {
-            ball.destroyBall();
             Instantiate(ballPrefab, new Vector3(0.01632249f, 2.535f, 0), Quaternion.identity);
         }
     }
     
     public void GameOver()
     {
-        //Game Over Screen
+        mainCanvas.enabled = false;
+        gameOverCanvas.enabled = true;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("MainScene");
     }
 
 }
